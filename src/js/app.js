a56c394f3c87103436fc1a737ad489fb89b69345
Facundo <saiegh@gmail.com>
2017-06-20 23:46:16 -0300

    hay q instalar el plugin de cordova "barcodescanner"

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 var tiempoSplash=1500;
-
-
+var scannerQR;
+var dataDinos;
 
 $(document).ready(function() {
     // are we running in native app or in a browser?
@@ -34,9 +34,56 @@ function onDeviceReady() {
     }, tiempoSplash); //splash a la fuerza 1 seg
 
 
+  if(window.isphone)  scannerQR=cordova.plugins.barcodeScanner;
+  //click en el boton scanner
+  $("#scanner").click(function(){
+     cordova.plugins.barcodeScanner.scan(function(data) {
+        QRok(data);
+      }, function(error) {
+        QRmal(error);
+      });
+  });
+
+
+   $("#lista").click(function(){
+      console.log("lista");
+   });
+
+   $("#minijuegos").click(function(){
+    console.log("minijuegos");
+   });
+
+   $("#info").click(function(){
+    console.log("info");
+   });
+
+
+    $.ajax({
+          url: "dataDinos.json",
+          dataType: 'json',
+          success: function(json) {
+             dataDinos=json;
+          }});
 
   
 }
+
+function encontrarDinoPorNombre(nombre){
+  for(var i=0; i<dataDinos.length;i++){
+    if(nombre.toLowerCase()==dataDinos[i].nombre.toLowerCase()){
+      return dataDinos[i];
+    }
+  }
+  return -1;
+}
+
+function QRok(data){
+  alert(data.text);
+}
+function QRmal(error){
+  console.log(error);
+}
+
 
 function responsive(){
   console.log("responsive");
